a0387f59ffd0897af74bec483173dfe198be2677
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-01-07 18:55:28 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use std::env;
+use std::fs::File;
+use std::io::{stdin, stdout, Write};
+use std::io::{self, BufRead};
+use colored::Colorize;
+use regex::Regex;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    let contents = match File::open(filename){
+        Err(why) => panic!("couldn't open {}: ", why),
+        Ok(file) => file,
+    };
+
+    let fivelines = io::BufReader::new(&contents).lines().into_iter().take(5);
+
+    for (index, line) in fivelines.enumerate() {
+        if let Ok(line) = line {
+            println!("{}| {}", index+1, line);
+        }
+    }
+
+    loop {
+        print!("> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let mut args = parts;
+
+        match command {
+            "line" => {
+                let contents = match File::open(filename){
+                    Err(why) => panic!("couldn't open {}: ", why),
+                    Ok(file) => file,
+                };
+
+                // let lines = io::BufReader::new(contents).lines().into_iter();
+                let lines = io::BufReader::new(contents).lines().into_iter();
+
+                let number_str = args.next().unwrap();
+                let num_connect_re = Regex::new(r"([0-9]*)-([0-9]*)").unwrap();
+                if num_connect_re.is_match(&number_str){
+                    let caps = num_connect_re.captures(number_str).unwrap();
+
+                    let first_num = caps.get(1).map_or("", |m| m.as_str());
+                    let first_num: usize = first_num.parse().unwrap();
+
+                    let last_num = caps.get(2).map_or("", |m| m.as_str());
+                    let last_num: usize = last_num.parse().unwrap();                    
+
+                    for (index, line) in lines.enumerate(){
+                        if let Ok(line) = line {
+                            let num = index + 1;
+                            if first_num <= num && num <= last_num  {
+                                println!("{}| {}", format!("{}", index+1).bold().blue(), line);
+                            }
+                        }
+                    }
+                } else {
+                let num_re = Regex::new(r"([0-9]*)").unwrap();
+                if num_re.is_match(&number_str) {
+                    let caps = num_re.captures(number_str).unwrap();
+                    let num = caps.get(1).map_or("", |m| m.as_str());
+                    let num = num.parse().unwrap();
+
+                    for (index, line) in lines.enumerate() {
+                    if let Ok(line) = line {
+                        if (index + 1) == num {
+                            println!("{}| {}", format!("{}", num).bold().blue(), line);
+                        }
+                        }
+                    }
+                }
+            }
+
+            },
+            "exit" => {
+                return
+            },
+            command =>{
+                println!("put other command")
+            }
+
+
+        }
+
+
+    }
+
+}
